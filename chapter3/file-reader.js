f121aff1d06bac5e24bdcf86f3f6631f82daa974
Chris <chris.li@xerox.com>
2014-12-16 16:10:52 +0800

    An Incorrect Attempt at Asynchronous Error Handling

--- chapter3/file-reader.js
+++ chapter3/file-reader.js
@@ -1,29 +1,12 @@
 var fs = require('fs');
-var filename = "foo.txt";
 
-function cbReadFile(error, data) {
-  if (error) {
-    throw error;
-  }
-
-  console.log(data);
+try {
+  fs.readFile("", "utf8", function(error, data) {
+    if (error) {
+      throw error;
+    }
+    console.log(data);
+  });
+} catch (exception) {
+  console.log("The exception was caught!");
 }
-
-function cbStat(error, stats) {
-  if (error) {
-    throw error;
-  }
-
-  if (stats.isFile()) {
-    fs.readFile(fileName, "utf8", cbReadFile);
-  }
-
-}
-
-function cbExists(exists) {
-  if (exists) {
-    fs.stat(filename, cbStat);
-  }
-}
-
-fs.exists(filename, cbExists);
