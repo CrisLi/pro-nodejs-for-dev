df8130481b0a87bc8dabbfa4d795dba404e6621f
Chris <chris.li@xerox.com>
2014-12-16 16:13:23 +0800

    An Example of Global Exception Handler

--- chapter3/file-reader.js
+++ chapter3/file-reader.js
@@ -1,12 +1,12 @@
 var fs = require('fs');
 
-try {
-  fs.readFile("", "utf8", function(error, data) {
-    if (error) {
-      throw error;
-    }
-    console.log(data);
-  });
-} catch (exception) {
-  console.log("The exception was caught!");
-}
+fs.readFile("", "utf8", function(error, data) {
+  if (error) {
+    throw error;
+  }
+  console.log(data);
+});
+
+process.on("uncaughtException", function(error) {
+  console.log("The exception was caught");
+});
