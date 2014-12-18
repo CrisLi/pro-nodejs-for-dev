112118ebfe3c4f33b5b04445379bd7c5cbe10170
Chris <chris.li@xerox.com>
2014-12-18 10:06:16 +0800

    Creating an Object That Extends EventEmitter

---
+++ chapter4/extends-event-emitter.js
@@ -0,0 +1,25 @@
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+function UserEventEmitter() {
+  EventEmitter.call(this);
+
+  this.addUser = function(username, password) {
+    // add the user
+    // then emit an event
+    this.emit("userAdded", username, password);
+  };
+}
+
+util.inherits(UserEventEmitter, EventEmitter);
+
+var user = new UserEventEmitter();
+var username = "Chris";
+var password = "password";
+
+user.on("userAdded", function(username, password) {
+  console.log("Added user " + username);
+});
+
+user.addUser(username, password);
+console.log(user instanceof EventEmitter);
